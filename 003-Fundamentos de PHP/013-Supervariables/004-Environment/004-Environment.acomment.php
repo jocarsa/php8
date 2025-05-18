8e212b29fbb09092fcee19a86257ff486be0c898
Jose Vicente Carratala <info@josevicentecarratala.com>
2025-05-19 00:25:19 +0200

    1

--- 003-Fundamentos de PHP/013-Supervariables/004-Environment/004-Environment.acomment.php
+++
@@ -1,7 +0,0 @@
-La función `var_dump($_ENV);` en PHP muestra información sobre la variable superglobal `$_ENV`. Esta variable contiene información sobre las variables de entorno del sistema donde se está ejecutando el script PHP.
-
-Las variables de entorno son valores dinámicos y específicos del sistema operativo que pueden ser utilizados por aplicaciones y programas. Estas variables pueden contener información sobre la configuración del sistema, rutas de archivos, preferencias del usuario, entre otros.
-
-La salida de `var_dump($_ENV);` mostrará una lista detallada de todas las variables de entorno disponibles en el entorno donde se está ejecutando el script PHP. Esto incluye variables como `PATH`, `HOME`, `USER`, `LANG`, entre otras, dependiendo del entorno y del sistema operativo en el que se está ejecutando el script.
-
-La información mostrada por `var_dump($_ENV);` incluirá las claves y sus valores correspondientes para las variables de entorno presentes en el sistema. Esto puede ser útil para acceder a información específica del entorno de ejecución del servidor donde se está ejecutando el script PHP.
